dc8be9378457c70c824702f236e47ec188900b65
Jonas Guibert <Jonas@MacBook-Pro-de-Jonas.local>
2021-10-25 17:39:42 +0200

    code generalized for more data/problem to solve on the edge

--- GRB.cpp
+++ GRB.cpp
@@ -6,17 +6,17 @@
 #include "TApplication.h"
 #include "TDirectory.h"
 #include "TTree.h"
+#include "TProfile.h"
 #include<iostream>
 using namespace std;
 
-void PULL(){
-    TApplication *a = new TApplication("a", 0, 0);
-    double bin=0.25;//quarter of second
-    int jump=5; //jump before and after the bean evaluated (in second)
-    int MeanWidth=25; // nbr of second used to make the mean for the estimation
-    int TShift=1300; // Time shift for the histogram
-    double timecheck=0; //used to check if it's one single event
-    TFile * input= new TFile("~/Desktop/Uni/Master/Astrophysics_Lab_I/Automne/scrate.root","READ");
+TFile* PULL(double pull_max=8,TString filename="~/Desktop/Uni/Master/Astrophysics_Lab_I/Automne/scrate.root"){
+    const double bin_size=1.0;//search bin size seconds
+    const int jump=5; //jump in bins before and after for doing mean
+    const int MeanWidth=25; // nbr of bins to do the mean estimation
+    const int TShift=100000; // Time window in bins
+    const double Time_GRB = 1488809240;
+    TFile * input= new TFile(filename,"READ");
     TTree * tree=(TTree*)input->Get("f1rate");
     double unix_time;
     tree->SetBranchAddress("unix_time",&unix_time);
@@ -25,6 +25,123 @@ void PULL(){
     double rate_err[14];
     tree->SetBranchAddress("rate_err", rate_err);
     TFile *output = new TFile("out.root","RECREATE");
+    
+    
+    
+    TProfile *h = new TProfile("h","hist with right error", TShift/bin_size,-TShift/2,TShift/2);
+    TH1D *h1 = new TH1D("h1","hist of the mean", TShift/bin_size,-TShift/2,TShift/2);
+    TH1D *h2 = new TH1D("h2","hist data-mean", TShift/bin_size,-TShift/2,TShift/2);
+    TH1D *h3 = new TH1D("h3","hist pull", TShift/bin_size,-TShift/2,TShift/2);
+    TH1D *e = new TH1D("e","hist of the rate_err^2", TShift/bin_size,-TShift/2,TShift/2);
+    TH1D *p = new TH1D("p","hist of pull values", 100,-10,40);
+    
+    const Long64_t first_entry=33000000;
+    const Long64_t last_entry=34000000;//tree->GetEntries()
+
+    for (Long64_t i=first_entry; i<last_entry; i++){
+        tree->GetEntry(i);
+        h->Fill(unix_time-Time_GRB, rate[13]);
+        e->Fill(unix_time-Time_GRB, rate_err[13]*rate_err[13]);
+    }
+    for (int i=0 ;i<h->GetNbinsX();i++){
+        int n_entry=h->GetBinEntries(i+1);
+        if (n_entry!=0){
+            e->SetBinContent(i+1,sqrt(e->GetArray()[i+1])/n_entry);
+        }
+    }
+    
+    double* mean=new double[3];
+    double* t=new double[3];
+    double timecheck=0;
+    double tot_pull=0;
+    double a,b,slope,val,pull;
+    int na,nb;
+    bool in_a_grb=false;
+    for (int j=jump+MeanWidth;j<h->GetNbinsX()-jump-MeanWidth;j++){
+        if (h->GetBinEntries(j)!=0){
+            a =0;
+            b =0;
+            na=0;
+            nb=0;
+            for(int i=0;i<MeanWidth;i++){
+                if (h->GetBinEntries(j+jump+i+1)!=0){
+                    a+=h->GetBinContent(j+jump+i+1);
+                    na++;
+                }
+            }
+            for(int i=0;i<MeanWidth;i++){
+                if (h->GetBinEntries(j-jump-i+1)!=0){
+                    b+=h->GetBinContent(j-jump-i+1);
+                    nb++;
+                }
+            }
+            if (na<10) continue;
+            if (nb<5) continue;
+            if (na!=0){
+                mean[0]=a/na;
+            }
+            if (nb!=0){
+                mean[2]=b/nb;
+            }
+
+            t[0]=h->GetBinLowEdge(j+jump+MeanWidth/2);
+            t[1]=h->GetBinLowEdge(j);
+            t[2]=h->GetBinLowEdge(j-jump-MeanWidth/2);
+            slope=(mean[2]-mean[0])/(t[2]-t[0]);
+            mean[1]=slope*(t[1]-t[0])+mean[0];
+            val=(h->GetBinContent(j+1)-mean[1]);
+        
+            pull=val;
+            if (e->GetBinContent(j+1)!=0){
+                pull/=e->GetBinContent(j+1);
+            }
+            h1->Fill(t[1],mean[1]);
+            h2->Fill(t[1],val);
+            h3->Fill(t[1],pull);
+            p->Fill(pull);
+            if (pull>pull_max){
+                if (!in_a_grb) {
+                    timecheck=t[1];
+                    in_a_grb=true;
+                }
+                tot_pull+=pull;
+            }
+            else{
+                if (in_a_grb){
+                    cout<<"anomaly at unix time: "<< timecheck << " duration "<<t[1]-timecheck<<" tot pull "<<tot_pull<< endl;
+                    tot_pull=0;
+                    in_a_grb=false;
+                }
+            }
+        }
+        else{
+            if (in_a_grb){
+                cout<<"anomaly at unix time: "<< timecheck << " duration "<<t[1]-timecheck<<" tot pull "<<tot_pull<< endl;
+                tot_pull=0;
+                in_a_grb=false;
+                
+            }
+        }
+    }
+    delete[] mean;
+    delete[] t;
+    
+    output->cd();
+    h->Write();
+    e->Write();
+    p->Write();
+    h1->Write();
+    h2->Write();
+    h3->Write();
+    //c1->Write();
+    return output;
+}
+
+
+int main(){
+    TApplication *a = new TApplication("a", 0, 0);
+    TFile* f=PULL();
+    
     
     TCanvas *c1 = new TCanvas("c1","Histogram Drawing Options",200,10,700,1200);
     TPad *pad1 = new TPad("pad1","The pad with the function",0.1,0.75,0.9,1);
@@ -35,71 +152,18 @@ void PULL(){
     pad2->Draw();
     pad3->Draw();
     pad4->Draw();
-    
-    TH1D *h = new TH1D("h","hist with right error", TShift,-TShift/2,TShift/2);
-    TH1D *h1 = new TH1D("h1","hist of the mean", TShift,-TShift/2,TShift/2);
-    TH1D *h2 = new TH1D("h2","hist data-mean", TShift,-TShift/2,TShift/2);
-    TH1D *h3 = new TH1D("h3","hist pull", TShift,-TShift/2,TShift/2);
-    TH1D *e = new TH1D("e","hist of the rate_err^2", TShift,-TShift/2,TShift/2);
-    TH1D *p = new TH1D("p","hist of pull values", 100,-10,40);
-    for (int i=0; i< tree->GetEntries(); i++){
-        if (i < 33000000) continue;
-        if (i > 33000000 + 1000000) continue;
-        tree->GetEntry(i);
-        h->Fill(unix_time-1488809240, rate[13]*bin);
-        e->Fill(unix_time-1488809240, rate_err[13]*rate_err[13]);
-    }
-    double* er=new double[e->GetNbinsX()+2];
-    for (int i=0 ;i<TShift+2;i++) er[i]=sqrt(e->GetArray()[i])*bin;
-    h->SetError(er);
-    
-
-
-    for (int j=jump+MeanWidth;j<TShift+2-MeanWidth-jump;j++){
-        double a =0;
-        double b =0;
-        for(int i=0;i<MeanWidth;i++) a+=h->GetArray()[j+jump+i];
-        for(int i=0;i<MeanWidth;i++) b+=h->GetArray()[j-jump-i];
-        double* mean=new double[3];
-        double* t=new double[3];
-        mean[0]=a/MeanWidth;
-        mean[2]=b/MeanWidth;
-        t[0]=j+jump+MeanWidth/2;
-        t[1]=j;
-        t[2]=j-jump-MeanWidth/2;
-        double slope=(mean[2]-mean[0])/(t[2]-t[0]);
-        mean[1]=slope*(t[1]-t[0])+mean[0];
-        double pull=(h->GetArray()[j]-mean[1])/er[j];
-        h1->Fill(j-TShift/2+1,mean[1]);
-        p->Fill(pull);
-        delete[] mean;
-        delete[] t;
-        if (pull>8){
-            if (timecheck==0) {
-                timecheck=j;
-                cout<<"anomaly at unix time: "<< j<< " with a pull > 8"<< endl;
-            }
-            if (timecheck-j>20){
-                cout<<"anomaly at unix time: "<< j<< " with a pull > 8"<< endl;
-                timecheck=j;
-            }
-                
-            
-            
-        }
-        
-    }
-    for (int i=0;i<h->GetNbinsX();i++){
-        h2->Fill(i-(TShift/2+1),h->GetArray()[i]-h1->GetArray()[i]);
-        }
-    
-    for (int i=0;i<h->GetNbinsX();i++){
-        h3->Fill(i-(TShift/2+1),h2->GetArray()[i]/er[i]);
-        }
+    TH1D* h=(TH1D*)f->Get("h");
+    TH1D* h1=(TH1D*)f->Get("h1");
+    TH1D* h2=(TH1D*)f->Get("h2");
+    TH1D* h3=(TH1D*)f->Get("h3");
+    TH1D* p=(TH1D*)f->Get("p");
+    TH1D* e=(TH1D*)f->Get("e");
+    /*
     h->GetXaxis()->SetRangeUser(-(TShift/2-jump-MeanWidth-1), TShift/2-jump-MeanWidth);
     h1->GetXaxis()->SetRangeUser(-(TShift/2-jump-MeanWidth-1), TShift/2-jump-MeanWidth);
     h2->GetXaxis()->SetRangeUser(-(TShift/2-jump-MeanWidth-1), TShift/2-jump-MeanWidth);
     h3->GetXaxis()->SetRangeUser(-(TShift/2-jump-MeanWidth-1), TShift/2-jump-MeanWidth);
+     */
     pad1->cd();
     h->Draw();
     pad2->cd();
@@ -108,22 +172,10 @@ void PULL(){
     h2->Draw("hist");
     pad4->cd();
     h3->Draw("hist");
-    
-    output->cd();
-    h->Write();
-    e->Write();
-    p->Write();
-    h1->Write();
+    c1->Update();
     c1->Write();
-    output->Close();
-    delete a ;
-}
-
-
-int main(){
-
-    PULL();
-
+    f->Close();
+    delete a;
 return 0;
 }
 //tree->Draw("unix_time-1488809240>>h","rate[10]/4","",1000000,33000000);
@@ -131,4 +183,6 @@ return 0;
 TH1D *h = new TH1D("h","a trial histogram", 100, -5, 5);
 for (Int_t i = 0; i < 10000; i++) h->Fill(gRandom->Gaus(0, 1));
 cout<<"Hello"<<endl;
+ 
+ 
  */
